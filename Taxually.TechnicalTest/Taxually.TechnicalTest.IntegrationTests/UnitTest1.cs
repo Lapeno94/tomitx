acb030fb0854e64a36ee19f23ca333d971a9d947
Tomi <lapeno94@gmail.com>
2023-01-02 16:47:42 +0100

    add skeleton projects and initial commit

---
+++ Taxually.TechnicalTest/Taxually.TechnicalTest.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace Taxually.TechnicalTest.IntegrationTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
---
+++ Taxually.TechnicalTest/Taxually.TechnicalTest.UnitTests/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace Taxually.TechnicalTest.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
